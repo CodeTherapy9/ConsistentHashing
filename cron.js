f29c8a821eabb6a4de7ba3ff62396906f3d7a53f
byjus-aritro-roy <aritro.roy@byjus.com>
2023-03-13 21:17:22 +0530

    edits

--- cron.js
+++ cron.js
@@ -3,11 +3,12 @@ const cron = require('cron').CronJob
 
 const job = new cron(
 
-	'*/2 * * * * *',
+	'*/20 * * * * *',
 	async function() {
 		console.log('running health check every 20 seconds to check active servers . . .');
         const hashRing = await createOrUpdateHashRing()
         const dataMap = {}
+        console.log(hashRing.length)
         if (hashRing.length > 0) {
             const s = (Number(hashRing[0].split("#")[0]) + 1e9 + 7).toString() + "#" + hashRing[0].split("#")[1]
             hashRing.push(s)
